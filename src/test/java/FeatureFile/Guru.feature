642555af2ec93939089bf5ecf382221e24946bfb
Aj-journey <abhijeetjadhav.m80@gmail.com>
2025-01-24 12:55:19 +0530

    Feature_Title_Url_Validated

--- src/test/java/FeatureFile/Guru.feature
+++ src/test/java/FeatureFile/Guru.feature
@@ -3,3 +3,5 @@ Feature: Guru99 Application
 Scenario: Validate Registration Functionality
 
 Given user open guruApplication in "chrome" browser
+When user validate url
+And user validate title
--- src/test/java/StepDefination/RegistrationStep.java
+++ src/test/java/StepDefination/RegistrationStep.java
@@ -1,8 +1,11 @@
 package StepDefination;
 
+import org.testng.Assert;
+
 import com.Guru.BaseLayer.BaseClass;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class RegistrationStep extends BaseClass {
 	
@@ -10,7 +13,22 @@ public class RegistrationStep extends BaseClass {
 	public void user_open_guru_application_in_browser(String browserName) {
 		
 		BaseClass.initialization(browserName);
-	   
-
+	  
 }
+	
+	@When("user validate url")
+	public void user_validate_url() {
+	  
+		String url=getDriver().getCurrentUrl();
+		
+		Assert.assertEquals(url.contains("newtours"), true);
+	}
+	@When("user validate title")
+	public void user_validate_title() {
+	    
+		String title=getDriver().getTitle();
+		
+		Assert.assertEquals(title.contains("Tours"), true);
+	}
+
 }
